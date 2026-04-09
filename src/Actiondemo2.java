ba42227f491b2138aa60056e4f368669323b2083
Krishan Kumar <jha.kumar.krishan@gmail.com>
2026-04-09 17:36:37 +0530

    Updated existing file logic

--- src/Actiondemo2.java
+++ src/Actiondemo2.java
@@ -2,6 +2,7 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
@@ -17,8 +18,9 @@ public class Actiondemo2 {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(10));
 		driver.get("https://www.amazon.in/");
+		WebElement move=driver.findElement(By.cssSelector("div[id='nav-link-accountList']"));
         Actions action=new Actions(driver);
-        action.moveToElement(driver.findElement(By.id("nav-link-accountList"))).build().perform();
+        action.moveToElement(move).build().perform();
         
 		
 		
