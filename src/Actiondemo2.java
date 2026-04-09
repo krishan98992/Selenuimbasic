7479e24b2d93210ca4bcfac859be3eb4ba8980b5
Krishan <jha.kumar.krishan@gmail.com>
2026-04-10 01:08:47 +0530

    update

--- src/Actiondemo2.java
+++ src/Actiondemo2.java
@@ -1,6 +1,7 @@
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -20,9 +21,12 @@ public class Actiondemo2 {
 		driver.get("https://www.amazon.in/");
 		WebElement move=driver.findElement(By.cssSelector("div[id='nav-link-accountList']"));
         Actions action=new Actions(driver);
+        //to specific element
         action.moveToElement(move).build().perform();
         
-		
+		action.moveToElement(driver.findElement(By.cssSelector("input[id='twotabsearchtextbox']"))).click().keyDown(Keys.SHIFT).doubleClick().sendKeys("mobile").build().perform();
+		/*cancetting action . composite action
+		 */
 		
 
 	}
