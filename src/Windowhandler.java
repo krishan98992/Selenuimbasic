67e0459e830460615fe0ac04a6a34e85ebff9641
Krishan Kumar Jha <krishankumar@tradesocio.com>
2026-04-15 17:19:10 +0530

    update

--- src/Windowhandler.java
+++ src/Windowhandler.java
@@ -1,8 +1,23 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Windowhandler {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(10));
+		
+		driver.get("https://rahulshettyacademy.com/loginpagePractise/");
+		driver.findElement(By.cssSelector(".blinkingText")).click();
+		
 
 	}
 
