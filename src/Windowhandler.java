5e9f939a64aeb1d70d72b23ad52fdbca3100b533
Krishan Kumar Jha <krishankumar@tradesocio.com>
2026-05-18 16:12:54 +0530

    update

--- src/Windowhandler.java
+++ src/Windowhandler.java
@@ -1,4 +1,6 @@
 import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -17,8 +19,22 @@ public class Windowhandler {
 		
 		driver.get("https://rahulshettyacademy.com/loginpagePractise/");
 		driver.findElement(By.cssSelector(".blinkingText")).click();
-		
+		Set<String> window=driver.getWindowHandles(); //Parent and child window
+	     Iterator<String> it= window.iterator();
+	     String parentid=it.next();
+	     String childid=it.next();
+	     System.out.println(childid);
+	     driver.switchTo().window(childid);
 
-	}
+		System.out.println(driver.findElement(By.cssSelector(".im-para.red")).getText());
+		String emailid=driver.findElement(By.cssSelector(".im-para.red")).getText().split("at")[1].trim().split(" ")[0];
+		
+	     		System.out.println(emailid);
+	     		
+	     		driver.switchTo().window(parentid);
+	     		driver.findElement(By.id("username")).sendKeys(emailid);
+	     		
+	    
+		}
 
 }
